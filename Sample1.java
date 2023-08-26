b7a45372f6506b92c25aa7a635bd7fafc117c5c7
maeda yuki <yuhkimaemae@gmail.com>
2023-08-27 01:09:57 +0900

    複数の数字の入力を受け取るプログラム

---
+++ Sample1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Sample1 {
+    public static void main(String[] args) throws Exception {
+            Scanner scanner = new Scanner(System.in);
+        
+        String input = scanner.nextLine();
+        String[] string = input.split(" ");
+        int[] num = new int[string.length];
+        
+        
+        for(int i=0;i<string.length;i++){
+            num[i]=Integer.parseInt(string[i]);
+            System.out.println(num[i]);
+        }
+        scanner.close();
+}
+}
